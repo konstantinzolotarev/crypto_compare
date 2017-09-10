f673f4d899576df6605d149ba92c4571703ba284
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-09-10 20:34:06 +0300

    updated

--- lib/crypto_compare.ex
+++ lib/crypto_compare.ex
@@ -1,18 +1,32 @@
 defmodule CryptoCompare do
-  @moduledoc """
-  Documentation for CryptoCompare.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  use HTTPoison.Base
 
-  ## Examples
+  @host "https://www.cryptocompare.com/api/"
+  @timeout Application.get_env(:crypto_compare, :request_timeout, 8000)
 
-      iex> CryptoCompare.hello
-      :world
+  def host, do: @host
+  def process_url(url), do: @host <> url
 
-  """
-  def hello do
-    :world
+  defp process_request_options([]), do: [timeout: @timeout]
+  defp process_request_options([timeout: _t] = opts), do: opts
+  defp process_request_options(opts), do: Keyword.merge(opts, [timeout: @timeout])
+
+  defp process_request_body(req) when is_binary(req), do: req
+  defp process_request_body(req), do: Poison.encode!(req)
+
+  defp process_response_body(""), do: ""
+  defp process_response_body(body) do
+    body
+    |> Poison.decode!(keys: :atoms)
   end
+
+  def get_body(url), do: get(url) 
+  def get_body(url, pid), do: get(url, %{}, stream_to: pid)
+
+  def post_body(url, params, headers) do
+    post(url, params, headers)
+  end
+
 end
