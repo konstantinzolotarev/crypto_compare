5d29deec52c1515519041f45a8d10376656f1131
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-09-18 16:58:55 +0300

    Added pricemultifull

--- lib/crypto_compare.ex
+++ lib/crypto_compare.ex
@@ -123,4 +123,101 @@ defmodule CryptoCompare do
  
   @doc false
   def pricemulti(fsyms, tsyms, params), do: ApiMini.get_body("pricemulti", [fsyms: fsyms, tsyms: tsyms] ++ params)
+
+
+  @doc """
+  Get all the current trading info (price, vol, open, high, low etc) of any list of cryptocurrencies
+  in any other currency that you need.If the crypto does not trade directly into the toSymbol requested, 
+  BTC will be used for conversion. This API also returns Display values for all the fields. 
+  If the opposite pair trades we invert it (eg.: BTC-XMR).
+
+  Optional parameters: 
+   - `e` - String. Name of exchange. Default: CCCAGG
+   - `extraParams` - String. Name of your application
+   - `sign` - bool. If set to true, the server will sign the requests.
+   - `tryConversion` - bool. If set to false, it will try to get values without using any conversion at all. Default: `true`
+
+  Example: 
+
+  ```elixir
+  iex(16)> CryptoCompare.pricemultifull(["ETH", "DASH"], ["BTC", "USD"], [extraParams: "my app"])
+  {:ok,
+  %{DISPLAY: %{DASH: %{BTC: %{CHANGE24HOUR: "Ƀ 0.00083",
+         CHANGEPCT24HOUR: "1.01", FROMSYMBOL: "DASH", HIGH24HOUR: "Ƀ 0.08821",
+         LASTMARKET: "Kraken", LASTTRADEID: 1505742927.3238,
+         LASTUPDATE: "Just now", LASTVOLUME: "DASH 1",
+         LASTVOLUMETO: "Ƀ 0.08374", LOW24HOUR: "Ƀ 0.08130",
+         MARKET: "CryptoCompare Index", MKTCAP: "Ƀ 628.13 K",
+         OPEN24HOUR: "Ƀ 0.08218", PRICE: "Ƀ 0.08301",
+         SUPPLY: "DASH 7,566,875.1", TOSYMBOL: "Ƀ",
+         VOLUME24HOUR: "DASH 66,103.7", VOLUME24HOURTO: "Ƀ 5,598.87"},
+       USD: %{CHANGE24HOUR: "$ 36.1", CHANGEPCT24HOUR: "11.99",
+         FROMSYMBOL: "DASH", HIGH24HOUR: "$ 340.91", LASTMARKET: "HitBTC",
+         LASTTRADEID: 34140863, LASTUPDATE: "Just now",
+         LASTVOLUME: "DASH 0.05400", LASTVOLUMETO: "$ 18.16",
+         LOW24HOUR: "$ 296.75", MARKET: "CryptoCompare Index",
+         MKTCAP: "$ 2,551.47 M", OPEN24HOUR: "$ 301.09", PRICE: "$ 337.19",
+         SUPPLY: "DASH 7,566,875.1", TOSYMBOL: "$",
+         VOLUME24HOUR: "DASH 33,876.6", VOLUME24HOURTO: "$ 10,855,035"}},
+     ETH: %{BTC: %{CHANGE24HOUR: "Ƀ 0.0041", CHANGEPCT24HOUR: "5.93",
+         FROMSYMBOL: "Ξ", HIGH24HOUR: "Ƀ 0.07573", LASTMARKET: "Poloniex",
+         LASTTRADEID: 34391845, LASTUPDATE: "Just now", LASTVOLUME: "Ξ 3",
+         LASTVOLUMETO: "Ƀ 0.2188", LOW24HOUR: "Ƀ 0.06866",
+         MARKET: "CryptoCompare Index", MKTCAP: "Ƀ 6,903.97 K",
+         OPEN24HOUR: "Ƀ 0.06883", PRICE: "Ƀ 0.07291",
+         SUPPLY: "Ξ 94,691,674.1", TOSYMBOL: "Ƀ",
+         VOLUME24HOUR: "Ξ 440,392.5", VOLUME24HOURTO: "Ƀ 31,872.6"},
+       USD: %{CHANGE24HOUR: "$ 43.02", CHANGEPCT24HOUR: "16.97",
+         FROMSYMBOL: "Ξ", HIGH24HOUR: "$ 301.17", LASTMARKET: "Gemini",
+         LASTTRADEID: 1729460950, LASTUPDATE: "Just now", LASTVOLUME: "Ξ 1.7",
+         LASTVOLUMETO: "$ 504.56", LOW24HOUR: "$ 251.78",
+         MARKET: "CryptoCompare Index", MKTCAP: "$ 28.08 B",
+         OPEN24HOUR: "$ 253.55", PRICE: "$ 296.57", SUPPLY: "Ξ 94,691,674.1",
+         TOSYMBOL: "$", VOLUME24HOUR: "Ξ 1,000,576.4",
+         VOLUME24HOURTO: "$ 279,431,015.1"}}},
+   RAW: %{DASH: %{BTC: %{CHANGE24HOUR: 8.299999999999974e-4,
+         CHANGEPCT24HOUR: 1.0099780968605467, FLAGS: "4", FROMSYMBOL: "DASH",
+         HIGH24HOUR: 0.08821, LASTMARKET: "Kraken",
+         LASTTRADEID: 1505742927.3238, LASTUPDATE: 1505742927,
+         LASTVOLUME: 1.00468613, LASTVOLUMETO: 0.0837405889355,
+         LOW24HOUR: 0.0813, MARKET: "CCCAGG", MKTCAP: 628126.2997194666,
+         OPEN24HOUR: 0.08218, PRICE: 0.08301, SUPPLY: 7566875.07191262,
+         TOSYMBOL: "BTC", TYPE: "5", VOLUME24HOUR: 66103.69737247002,
+         VOLUME24HOURTO: 5598.867303056865},
+       USD: %{CHANGE24HOUR: 36.10000000000002,
+         CHANGEPCT24HOUR: 11.989770500514805, FLAGS: "2", FROMSYMBOL: "DASH",
+         HIGH24HOUR: 340.91, LASTMARKET: "HitBTC", LASTTRADEID: 34140863,
+         LASTUPDATE: 1505742916, LASTVOLUME: 0.054, LASTVOLUMETO: 18.15534,
+         LOW24HOUR: 296.75, MARKET: "CCCAGG", MKTCAP: 2551474605.4982166,
+         OPEN24HOUR: 301.09, PRICE: 337.19, SUPPLY: 7566875.07191262,
+         TOSYMBOL: "USD", TYPE: "5", VOLUME24HOUR: 33876.57931777,
+         VOLUME24HOURTO: 10855035.024934988}},
+     ETH: %{BTC: %{CHANGE24HOUR: 0.00408, CHANGEPCT24HOUR: 5.927647827981985,
+         FLAGS: "4", FROMSYMBOL: "ETH", HIGH24HOUR: 0.07573,
+         LASTMARKET: "Poloniex", LASTTRADEID: 34391845, LASTUPDATE: 1505742928,
+         LASTVOLUME: 3.00073068, LASTVOLUMETO: 0.21879227, LOW24HOUR: 0.06866,
+         MARKET: "CCCAGG", MKTCAP: 6903969.958106048, OPEN24HOUR: 0.06883,
+         PRICE: 0.07291, SUPPLY: 94691674.0928, TOSYMBOL: "BTC", TYPE: "5",
+         VOLUME24HOUR: 440392.4897805099, VOLUME24HOURTO: 31872.558630222768},
+       USD: %{CHANGE24HOUR: 43.01999999999998,
+         CHANGEPCT24HOUR: 16.967067639518824, FLAGS: "4", FROMSYMBOL: "ETH",
+         HIGH24HOUR: 301.17, LASTMARKET: "Gemini", LASTTRADEID: 1729460950,
+         LASTUPDATE: 1505742937, LASTVOLUME: 1.7, LASTVOLUMETO: 504.56,
+         LOW24HOUR: 251.78, MARKET: "CCCAGG", MKTCAP: 28082709785.7017,
+         OPEN24HOUR: 253.55, PRICE: 296.57, SUPPLY: 94691674.0928,
+         TOSYMBOL: "USD", TYPE: "5", VOLUME24HOUR: 1000576.356339929,
+         VOLUME24HOURTO: 279431015.074645}}}}}
+  ```
+  """
+  @spec pricemultifull(String.t | [String.t], String.t | [String.t], [tuple]) :: {:ok, map} | {:error, any}
+  def pricemultifull(fsyms, tsyms, params \\ [])
+
+  @doc false
+  def pricemultifull(fsyms, tsyms, params) when is_list(fsyms), do: pricemultifull(Enum.join(fsyms, ","), tsyms, params)
+
+  @doc false
+  def pricemultifull(fsyms, tsyms, params) when is_list(tsyms), do: pricemultifull(fsyms, Enum.join(tsyms, ","), params)
+
+  @doc false
+  def pricemultifull(fsyms, tsyms, params), do: ApiMini.get_body("pricemultifull", [fsyms: fsyms, tsyms: tsyms] ++ params)
 end
