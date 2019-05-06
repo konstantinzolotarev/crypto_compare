39d5fc4705d3ed87fd20ca878f8d071e5ec17abf
Steffel <steffel@nash.io>
2019-05-06 09:33:09 +0100

    update httpoison

--- .history/mix_20190506093116.exs
+++ .history/mix_20190506093116.exs
@@ -30,7 +30,7 @@ defmodule CryptoCompare.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.4"},
+      {:httpoison, "~> 0.13"},
       {:poison, "~> 3.1"},
       {:ex_doc, "~> 0.14", only: :dev}
     ]
